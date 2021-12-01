80a35927d27e053a6582e2b6d8988f0426473979
Miklos Maroti <mmaroti@gmail.com>
2021-12-01 01:50:25 -0600

    stupid formatting

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,68 @@ use theory::*;
 
 fn main() {
     let mut theory: Theory = Default::default();
-    let _mul = theory.add_variable("mul", 3);
-    let _inv = theory.add_variable("inv", 2);
-    let _one = theory.add_variable("one", 1);
+    let equ = theory.add_variable("equ", 3);
+    let mul = theory.add_variable("mul", 3);
+    let inv = theory.add_variable("inv", 2);
+    let one = theory.add_variable("one", 1);
+
+    theory.add_clause(
+        6,
+        vec![
+            (mul, false, vec![0, 1, 3]),
+            (mul, false, vec![3, 2, 4]),
+            (mul, false, vec![1, 2, 5]),
+            (mul, true, vec![0, 5, 4]),
+        ],
+    );
+
+    theory.add_clause(
+        6,
+        vec![
+            (mul, false, vec![1, 2, 3]),
+            (mul, false, vec![0, 3, 4]),
+            (mul, false, vec![0, 1, 5]),
+            (mul, true, vec![5, 2, 4]),
+        ],
+    );
+
+    theory.add_clause(
+        3,
+        vec![
+            (inv, false, vec![0, 1]),
+            (mul, false, vec![1, 0, 2]),
+            (one, true, vec![2]),
+        ],
+    );
+
+    theory.add_clause(2, vec![(one, false, vec![0]), (mul, true, vec![0, 1, 1])]);
+
+    theory.add_clause(
+        4,
+        vec![
+            (mul, false, vec![0, 1, 2]),
+            (mul, false, vec![0, 1, 3]),
+            (equ, true, vec![2, 3]),
+        ],
+    );
+
+    theory.add_clause(
+        3,
+        vec![
+            (inv, false, vec![0, 1]),
+            (inv, false, vec![0, 2]),
+            (equ, true, vec![1, 2]),
+        ],
+    );
+
+    theory.add_clause(
+        2,
+        vec![
+            (one, false, vec![0]),
+            (one, false, vec![1]),
+            (equ, true, vec![0, 1]),
+        ],
+    );
 
     theory.print()
 }
--- src/theory.rs
+++ src/theory.rs
@@ -15,74 +15,39 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-use std::fmt;
+use std::io::{Result, Write};
 
 #[derive(Clone, Debug)]
 pub struct Variable {
-    pub arity: u32,
+    pub arity: usize,
     pub name: String,
 }
 
 impl Variable {
-    pub fn new(name: &str, arity: u32) -> Self {
+    pub fn new(name: &str, arity: usize) -> Self {
         let name = name.to_string();
         Self { arity, name }
-    }
-
-    fn fmt<Iter>(&self, iter: Iter) -> VariableFmt<'_, Iter>
-    where
-        Iter: Clone + Iterator<Item = u32>,
-    {
-        let name = &self.name;
-        VariableFmt { name, iter }
-    }
-}
-
-struct VariableFmt<'a, Iter>
-where
-    Iter: Clone + Iterator<Item = u32>,
-{
-    name: &'a str,
-    iter: Iter,
-}
-
-impl<'a, Iter> fmt::Display for VariableFmt<'a, Iter>
-where
-    Iter: Clone + Iterator<Item = u32>,
-{
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(formatter, "{}(", self.name)?;
-        let mut first = true;
-        for x in self.iter.clone() {
-            if first {
-                first = false;
-            } else {
-                write!(formatter, ",")?;
-            }
-            write!(formatter, "x{}", x)?;
-        }
-        write!(formatter, ")")
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct Literal {
     variable: usize,
-    arity: u32,
     sign: bool,
-    vars: Vec<u32>,
+    vars: Vec<usize>,
+    arity: usize,
 }
 
 impl Literal {
-    pub fn new(variable: usize, sign: bool, arity: u32, vars: Vec<u32>) -> Self {
-        for &var in &vars {
+    pub fn new(variable: usize, sign: bool, vars: Vec<usize>, arity: usize) -> Self {
+        for &var in vars.iter() {
             assert!(var < arity);
         }
         Self {
             variable,
-            arity,
             sign,
             vars,
+            arity,
         }
     }
 }
@@ -92,6 +57,12 @@ pub struct Clause {
     literals: Vec<Literal>,
 }
 
+impl Clause {
+    fn new(literals: Vec<Literal>) -> Self {
+        Self { literals }
+    }
+}
+
 #[derive(Clone, Debug, Default)]
 pub struct Theory {
     variables: Vec<Variable>,
@@ -99,19 +70,85 @@ pub struct Theory {
 }
 
 impl Theory {
-    pub fn add_variable(&mut self, name: &str, arity: u32) -> usize {
+    pub fn add_variable(&mut self, name: &str, arity: usize) -> usize {
         assert!(!self.variables.iter().any(|v| v.name == name));
         let v = self.variables.len();
         self.variables.push(Variable::new(name, arity));
         v
     }
 
-    pub fn print(&self) {
-        println!("variables:");
-        for (i, v) in self.variables.iter().enumerate() {
-            println!("{}. {}", i, v.fmt(0..v.arity));
-            // println!("  {}: {}", i, v.fmt([0, 3, 2].iter().cloned()));
+    pub fn add_clause(&mut self, arity: usize, literals: Vec<(usize, bool, Vec<usize>)>) {
+        let mut used = vec![false; arity];
+        let literals = literals
+            .into_iter()
+            .map(|(v, s, m)| {
+                for &x in m.iter() {
+                    used[x] = true;
+                }
+                Literal::new(v, s, m, arity)
+            })
+            .collect();
+        assert!(used.iter().all(|&x| x));
+        self.clauses.push(Clause::new(literals));
+    }
+
+    fn write_arguments<Iter>(&self, out: &mut impl Write, iter: Iter) -> Result<()>
+    where
+        Iter: Iterator<Item = usize>,
+    {
+        write!(out, "(")?;
+        let mut first = true;
+        for x in iter {
+            if first {
+                first = false;
+            } else {
+                write!(out, ",")?;
+            }
+            write!(out, "x{}", x)?;
         }
-        println!("clauses:");
+        write!(out, ")")
+    }
+
+    fn write_variable(&self, out: &mut impl Write, var: &Variable) -> Result<()> {
+        write!(out, "{}", var.name)?;
+        self.write_arguments(out, 0..var.arity)
+    }
+
+    fn write_literal(&self, out: &mut impl Write, lit: &Literal) -> Result<()> {
+        write!(out, "{}", if lit.sign { '+' } else { '-' })?;
+        write!(out, "{}", self.variables[lit.variable].name)?;
+        self.write_arguments(out, lit.vars.iter().cloned())
+    }
+
+    fn write_clause(&self, out: &mut impl Write, cla: &Clause) -> Result<()> {
+        let mut first = true;
+        for lit in cla.literals.iter() {
+            if first {
+                first = false;
+            } else {
+                write!(out, " ")?;
+            }
+            self.write_literal(out, lit)?;
+        }
+        Ok(())
+    }
+
+    pub fn print(&self) {
+        let out = std::io::stdout();
+        let mut out = out.lock();
+        let steps = || -> std::io::Result<()> {
+            for v in self.variables.iter() {
+                write!(out, "variable: ")?;
+                self.write_variable(&mut out, v)?;
+                writeln!(out)?;
+            }
+            for c in self.clauses.iter() {
+                write!(out, "clause: ")?;
+                self.write_clause(&mut out, c)?;
+                writeln!(out)?;
+            }
+            Ok(())
+        }();
+        steps.expect("Could not write to stdout");
     }
 }
