67d98ce9db77974e22ba1efc8c5d5228ba5fead6
Miklos Maroti <mmaroti@gmail.com>
2022-02-15 22:27:41 -0600

    small technical changes

--- src/solver2.rs
+++ src/solver2.rs
@@ -15,44 +15,72 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-use std::fmt;
+use std::rc::Rc;
+use std::{fmt, ptr};
 
 struct SolverItem<'a, ITEM: ?Sized>(&'a Solver, &'a ITEM);
 
 #[derive(Debug)]
 struct Domain {
-    name: String,
     size: usize,
+    name: String,
 }
 
 impl Domain {
     fn new(name: String, size: usize) -> Self {
         Self { name, size }
     }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn ptr_eq(&self, other: &Domain) -> bool {
+        ptr::eq(self, other)
+    }
+}
+
+fn get_coords(domains: &[Rc<Domain>], mut offset: usize, coords: &mut [usize]) {
+    debug_assert_eq!(domains.len(), coords.len());
+    for (size, coord) in domains
+        .iter()
+        .map(|dom| dom.size())
+        .zip(coords.iter_mut())
+        .rev()
+    {
+        *coord = offset % size;
+        offset /= size;
+    }
+    debug_assert_eq!(offset, 0);
+}
+
+fn get_offset(domains: &[Rc<Domain>], coords: &[usize]) -> usize {
+    debug_assert_eq!(domains.len(), coords.len());
+    let mut offset = 0;
+    for (size, &coord) in domains.iter().map(|dom| dom.size()).zip(coords.iter()) {
+        debug_assert!(coord < size);
+        offset *= size;
+        offset += coord;
+    }
+    offset
 }
 
 #[derive(Debug, Default, Clone)]
-struct Variable {}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct DomainIdx(usize);
+struct BooleanVariable {}
 
 #[derive(Debug)]
 struct Predicate {
     name: String,
-    domains: Box<[DomainIdx]>,
+    domains: Box<[Rc<Domain>]>,
     first_variable: usize,
     num_variables: usize,
 }
 
 impl Predicate {
-    fn new(solver: &Solver, name: String, domains: Vec<DomainIdx>) -> Self {
+    fn new(solver: &Solver, name: String, domains: Vec<Rc<Domain>>) -> Self {
         let domains = domains.into_boxed_slice();
         let first_variable = solver.num_variables();
-        let num_variables = domains
-            .iter()
-            .map(|&dom| solver.get_domain(dom).size)
-            .product();
+        let num_variables = domains.iter().map(|dom| dom.size).product();
         Self {
             name,
             domains,
@@ -66,17 +94,16 @@ impl Predicate {
     }
 }
 
-impl<'a> fmt::Display for SolverItem<'a, Predicate> {
+impl fmt::Display for Predicate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}(", self.1.name)?;
+        write!(f, "{}(", self.name)?;
         let mut first = true;
-        for &dom in self.1.domains.iter() {
+        for dom in self.domains.iter() {
             if first {
                 first = false;
             } else {
                 write!(f, ",")?;
             }
-            let dom = self.0.get_domain(dom);
             write!(f, "{}", dom.name)?;
         }
         write!(f, ")")
@@ -84,39 +111,19 @@ impl<'a> fmt::Display for SolverItem<'a, Predicate> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct PredicateIdx(usize);
+struct LiteralIdx(usize);
 
 #[derive(Debug)]
 struct AtomicFormula {
     sign: bool,
-    predicate: PredicateIdx,
+    predicate: Rc<Predicate>,
     variables: Box<[usize]>,
 }
 
 impl AtomicFormula {
-    fn new(
-        solver: &Solver,
-        domains: &mut Vec<Option<DomainIdx>>,
-        sign: bool,
-        predicate: PredicateIdx,
-        variables: Vec<usize>,
-    ) -> Self {
+    fn new(sign: bool, predicate: Rc<Predicate>, variables: Vec<usize>) -> Self {
         let variables = variables.into_boxed_slice();
-
-        let pred = solver.get_predicate(predicate);
-        assert_eq!(pred.domains.len(), variables.len());
-        for (pos, &var) in variables.iter().enumerate() {
-            if domains.len() <= var {
-                domains.resize(var + 1, None);
-            }
-            let dom1 = pred.domains[pos];
-            let dom2 = &mut domains[var];
-            if let Some(dom2) = dom2 {
-                assert_eq!(dom1, *dom2);
-            } else {
-                *dom2 = Some(dom1);
-            }
-        }
+        assert_eq!(predicate.arity(), variables.len());
 
         Self {
             sign,
@@ -126,12 +133,16 @@ impl AtomicFormula {
     }
 }
 
-impl<'a> fmt::Display for SolverItem<'a, AtomicFormula> {
+impl fmt::Display for AtomicFormula {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let pred = self.0.get_predicate(self.1.predicate);
-        write!(f, "{}{}(", if self.1.sign { '+' } else { '-' }, pred.name)?;
+        write!(
+            f,
+            "{}{}(",
+            if self.sign { '+' } else { '-' },
+            self.predicate.name
+        )?;
         let mut first = true;
-        for &var in self.1.variables.iter() {
+        for &var in self.variables.iter() {
             if first {
                 first = false;
             } else {
@@ -145,36 +156,54 @@ impl<'a> fmt::Display for SolverItem<'a, AtomicFormula> {
 
 #[derive(Debug)]
 struct UniversalFormula {
-    domains: Box<[DomainIdx]>,
+    domains: Box<[Rc<Domain>]>,
     disjunction: Box<[AtomicFormula]>,
 }
 
 impl UniversalFormula {
-    fn new(solver: &Solver, disjunction: Vec<(bool, PredicateIdx, Vec<usize>)>) -> Self {
-        let mut domains: Vec<Option<DomainIdx>> = Default::default();
+    fn new(disjunction: Vec<(bool, Rc<Predicate>, Vec<usize>)>) -> Self {
+        let mut domains: Vec<Option<Rc<Domain>>> = Default::default();
         let disjunction: Vec<AtomicFormula> = disjunction
             .into_iter()
-            .map(|(sign, pred, vars)| AtomicFormula::new(solver, &mut domains, sign, pred, vars))
+            .map(|(sign, pred, vars)| {
+                for (pos, &var) in vars.iter().enumerate() {
+                    if domains.len() <= var {
+                        domains.resize(var + 1, None);
+                    }
+                    let dom1 = &pred.domains[pos];
+                    let dom2 = &mut domains[var];
+                    if let Some(dom2) = dom2 {
+                        assert!(dom1.ptr_eq(dom2));
+                    } else {
+                        *dom2 = Some(dom1.clone());
+                    }
+                }
+                AtomicFormula::new(sign, pred, vars)
+            })
             .collect();
 
-        let domains: Vec<DomainIdx> = domains.into_iter().map(|d| d.unwrap()).collect();
+        let domains: Vec<Rc<Domain>> = domains.into_iter().map(|d| d.unwrap()).collect();
         Self {
             domains: domains.into_boxed_slice(),
             disjunction: disjunction.into_boxed_slice(),
         }
     }
+
+    fn arity(&self) -> usize {
+        self.domains.len()
+    }
 }
 
-impl<'a> fmt::Display for SolverItem<'a, UniversalFormula> {
+impl fmt::Display for UniversalFormula {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut first = true;
-        for atom in self.1.disjunction.iter() {
+        for atom in self.disjunction.iter() {
             if first {
                 first = false;
             } else {
                 write!(f, " | ")?;
             }
-            write!(f, "{}", SolverItem(self.0, atom))?;
+            write!(f, "{}", atom)?;
         }
         Ok(())
     }
@@ -182,16 +211,22 @@ impl<'a> fmt::Display for SolverItem<'a, UniversalFormula> {
 
 #[derive(Debug, Default)]
 pub struct Solver {
-    domains: Vec<Domain>,
-    variables: Vec<Variable>,
-    predicates: Vec<Predicate>,
+    domains: Vec<Rc<Domain>>,
+    variables: Vec<BooleanVariable>,
+    predicates: Vec<Rc<Predicate>>,
     formulas: Vec<UniversalFormula>,
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct DomainIdx(usize);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct PredicateIdx(usize);
 
 impl Solver {
     pub fn add_domain(&mut self, name: String, size: usize) -> DomainIdx {
         let idx = DomainIdx(self.domains.len());
-        self.domains.push(Domain::new(name, size));
+        self.domains.push(Rc::new(Domain::new(name, size)));
         idx
     }
 
@@ -199,13 +234,13 @@ impl Solver {
         self.variables.len()
     }
 
-    fn get_domain(&self, idx: DomainIdx) -> &Domain {
-        &self.domains[idx.0]
-    }
-
     pub fn add_predicate(&mut self, name: String, domains: Vec<DomainIdx>) -> PredicateIdx {
+        let domains: Vec<Rc<Domain>> = domains
+            .into_iter()
+            .map(|idx| self.domains[idx.0].clone())
+            .collect();
         let idx = PredicateIdx(self.predicates.len());
-        let pred = Predicate::new(self, name, domains);
+        let pred = Rc::new(Predicate::new(self, name, domains));
         self.variables.resize(
             self.variables.len() + pred.num_variables,
             Default::default(),
@@ -214,12 +249,12 @@ impl Solver {
         idx
     }
 
-    fn get_predicate(&self, idx: PredicateIdx) -> &Predicate {
-        &self.predicates[idx.0]
-    }
-
     pub fn add_formula(&mut self, disjunction: Vec<(bool, PredicateIdx, Vec<usize>)>) {
-        let formula = UniversalFormula::new(self, disjunction);
+        let disjunction: Vec<(bool, Rc<Predicate>, Vec<usize>)> = disjunction
+            .into_iter()
+            .map(|(sign, pred, vars)| (sign, self.predicates[pred.0].clone(), vars))
+            .collect();
+        let formula = UniversalFormula::new(disjunction);
         self.formulas.push(formula);
     }
 
@@ -228,10 +263,10 @@ impl Solver {
             println!("domain {} = {}", dom.name, dom.size);
         }
         for pred in self.predicates.iter() {
-            println!("predicate {}", SolverItem(self, pred));
+            println!("predicate {}", pred);
         }
         for form in self.formulas.iter() {
-            println!("formula {}", SolverItem(self, form));
+            println!("formula {}", form);
         }
     }
 }
